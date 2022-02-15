610f91668bcbc163505321df4848c849fc8918e6
Sen <89997819+BosoEnma@users.noreply.github.com>
2022-02-15 10:38:19 -0800

    Create If_Statements_In_Java.java

---
+++ If_Statements_In_Java.java
@@ -0,0 +1,11 @@
+package.com.company;
+
+public class If_Statements_In_Java{
+  public static void if_statements_in_java(String[]args){
+    int x = 123;
+    int y = 123;
+    if (x==y){
+      System.out.println("The Last Reality");
+}
+}
+} 
