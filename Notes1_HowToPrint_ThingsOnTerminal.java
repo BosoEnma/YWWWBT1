611606f8c17278a265de4e28ab6362b08d9578c4
Sen <89997819+BosoEnma@users.noreply.github.com>
2022-02-15 10:31:11 -0800

    Create Notes1_HowToPrint_ThingsOnTerminal.java

---
+++ Notes1_HowToPrint_ThingsOnTerminal.java
@@ -0,0 +1,7 @@
+package com.comany;
+public class Notes1_HowToPrint_ThingsOnTerminal{
+  public static void notes1_howtoprint_thingsonterminal(Strings[]args){
+    String TLR = "There is nothing in tis world to save don't try IO your fight with the seven is in vain all these worlds bow to my reign";
+    System.out.println(TLR);
+}
+}
