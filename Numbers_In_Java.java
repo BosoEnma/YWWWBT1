5965ad40e5aa59da123b40afec2a38c1fed34d08
Sen <89997819+BosoEnma@users.noreply.github.com>
2022-02-15 10:54:28 -0800

    Create Numbers_In_Java.java

---
+++ Numbers_In_Java.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main{
+  public static void main(String[]args){
+    int One_Billion 1000000000;
+    float Decimal_Number = 145.89F;
+    boolean Maybe = true;
+    String pp = "There is nothing in this world to save don't try IO";
+    char oneletter = 'C';
+}
+}
